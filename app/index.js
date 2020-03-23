7d4cc689c8290360fbea50c430c82074a7486016
CDLarsen <curtis.larssen@gmail.com>
2020-03-23 19:18:00 +1300

    1

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Popular from './components/Popular.js'
+import Popular from './components/Popular'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='container'>
         <Popular />
       </div>
     )
